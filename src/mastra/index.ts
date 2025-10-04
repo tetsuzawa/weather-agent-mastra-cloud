db7ef72ebcac71ed274aeeb44925627714cbbee0
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2025-10-04 18:28:10 +0900

    add memory

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,15 +1,17 @@
-import { Mastra } from '@mastra/core/mastra';
-import { PinoLogger } from '@mastra/loggers';
-import { weatherWorkflow } from './workflows';
-import { weatherAgent } from './agents';
+import { Mastra } from "@mastra/core/mastra";
+import { PinoLogger } from "@mastra/loggers";
+import { weatherWorkflow } from "./workflows";
+import { weatherAgent } from "./agents";
+import { Memory } from "@mastra/memory";
 
 export const mastra = new Mastra({
   workflows: { weatherWorkflow },
   agents: { weatherAgent },
   logger: new PinoLogger({
-    name: 'Mastra',
-    level: 'info',
+    name: "Mastra",
+    level: "info",
   }),
+  memory: new Memory(),
   observability: {
     default: {
       enabled: true,
